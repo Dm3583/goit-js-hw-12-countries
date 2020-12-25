8a00eda302caa2b6690a9dd89b18c0fadd3f1fb9
Dm3583 <pankovdm@gmail.com>
2020-12-25 15:33:48 +0200

    searchComplite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles.css';
 import './js/fetchCountries';
+import './js/search';
 import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
---
+++ src/js/search.js
@@ -0,0 +1,69 @@
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons.js';
+import refs from './refs';
+import countriesListTpl from '../templates/countriesList.hbs';
+import countryTpl from '../templates/countryInf.hbs';
+import fetchCountries from './fetchCountries';
+
+const debounce = require('lodash.debounce');
+
+const debouncedSearch = debounce(searchForQuery, 500);
+
+function notificationClose() {
+    const closeNotifyBtn = document.querySelectorAll('.ui-pnotify-closer');
+    if (closeNotifyBtn) {
+        closeNotifyBtn.forEach(e => e.click());
+    }
+};
+
+function clearResults() {
+    refs.root.innerHTML = '';
+};
+
+function renderElements(data, template) {
+    const markup = template(data);
+    refs.root.insertAdjacentHTML('afterbegin', markup);
+}
+
+function searchForQuery(e) {
+    let query = e.target.value;
+    if (query != '') {
+        fetchCountries(query)
+            .then(data => {
+                if (data.length > 10) {
+                    PNotify.error({
+                        text: "Too many matches found. Please enter a more specific query!",
+                        delay: 4000,
+                        shadow: true
+                    });
+                    clearResults();
+                    return;
+                }
+                if (data.length >= 2) {
+                    notificationClose();
+                    clearResults();
+                    const countries = data.map(el => el.name);
+                    renderElements(countries, countriesListTpl)
+                } else {
+                    notificationClose();
+                    clearResults();
+                    renderElements(data[0], countryTpl)
+                }
+            })
+            .catch(e => {
+                notificationClose();
+                clearResults();
+                PNotify.error({
+                    text: e,
+                    delay: 4000,
+                    shadow: true
+                });
+            });
+    } else {
+        clearResults();
+    }
+};
+
+refs.input.addEventListener('input', debouncedSearch);
+
+
