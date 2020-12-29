d0afa053e0f6dfdcd49a84b3f06f2252ffaea0e9
Dm3583 <pankovdm@gmail.com>
2020-12-29 17:09:09 +0200

    mistake correction

--- src/js/search.js
+++ src/js/search.js
@@ -4,8 +4,8 @@ import refs from './refs';
 import countriesListTpl from '../templates/countriesList.hbs';
 import countryTpl from '../templates/countryInf.hbs';
 import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
-const debounce = require('lodash.debounce');
 
 const debouncedSearch = debounce(searchForQuery, 500);
 
@@ -27,46 +27,41 @@ function renderElements(data, template) {
 
 function searchForQuery(e) {
     let query = e.target.value;
-    if (query != '') {
-        fetchCountries(query)
-            .then(data => {
-                if (data.length > 10) {
-                    PNotify.error({
-                        text: "Too many matches found. Please enter a more specific query!",
-                        delay: 4000,
-                        shadow: true
-                    });
-                    clearResults();
-                    return;
-                }
-                if (data.length >= 2) {
-                    notificationClose();
-                    clearResults();
-                    renderElements(data, countriesListTpl);
-                    data.forEach(el => {
-                        if (query.toLowerCase() === el.name.toLowerCase()) {
-                            renderElements(el, countryTpl);
-                        }
-                    });
-                } else {
-                    notificationClose();
-                    clearResults();
-                    renderElements(data[0], countryTpl)
-                }
-            })
-            .catch(e => {
-                notificationClose();
-                clearResults();
+    clearResults();
+    if (!query) {
+        notificationClose();
+        return;
+    }
+    fetchCountries(query)
+        .then(data => {
+            if (data.length > 10) {
                 PNotify.error({
-                    text: e,
+                    text: "Too many matches found. Please enter a more specific query!",
                     delay: 4000,
                     shadow: true
                 });
+                return;
+            }
+            if (data.length >= 2) {
+                renderElements(data, countriesListTpl);
+                data.forEach(el => {
+                    if (query.toLowerCase() === el.name.toLowerCase()) {
+                        renderElements(el, countryTpl);
+                    }
+                });
+            } else {
+                notificationClose();
+                renderElements(data[0], countryTpl)
+            }
+        })
+        .catch(e => {
+            notificationClose();
+            PNotify.error({
+                text: e,
+                delay: 4000,
+                shadow: true
             });
-    } else {
-        notificationClose();
-        clearResults();
-    }
+        });
 };
 
 refs.input.addEventListener('input', debouncedSearch);
